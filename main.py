0e75d61d38ae06880b13f2922e6a3f558a2d8b0d
Edbaro42 <102473154+Edbaro42@users.noreply.github.com>
2023-09-08 13:47:25 +0300

    Update main.py

--- main.py
+++ main.py
@@ -109,13 +109,7 @@ try:
 
         # Вставка данных в таблицу и агрегирование с использованием pandas
         aggregated_data = insert_data(cursor, api_response)
-
-        if api_response['count'] > 100:
-            limit = 100
-            offset += 100
-        else:
-            break
-			
+	
         # Запись данных в таблицу
         urls = f"INSERT IGNORE INTO {table_name} (DATE, URL, POSITION, IMPRESSIONS, CLICKS, CTR) " \
                 f"VALUES (%s, %s, %s, %s, %s, %s)"
@@ -124,6 +118,12 @@ try:
         cursor.executemany(urls, data_to_insert)
         # Сохранение изменений и закрытие соединения
         conn.commit()
+
+        if api_response['count'] > 100:
+            limit = 100
+            offset += 100
+        else:
+            break
 
         # Проверка условия выхода из цикла
         if offset > api_response['count']:
